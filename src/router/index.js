1ca8fda866b47597cc5ea0464b031ccbaeefbf55
luisricardo494 <luisricardo494@gmail.com>
2021-08-25 16:17:56 -0400

    changed title name

--- src/router/index.js
+++ src/router/index.js
@@ -82,7 +82,7 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  document.title = `${to.meta.title} | pinsBlog`;
+  document.title = `${to.meta.title} | PinBlogs`;
   next();
 });
 
